95f1dd4e32c49b30a03843baa71d2ea92b494a1c
Cody Uhi <codylkuhi@gmail.com>
2020-12-15 19:28:07 -0700

    Change http requests to use axios

--- src/rest-machinery/rest-machinery-service.ts
+++ src/rest-machinery/rest-machinery-service.ts
@@ -1,4 +1,4 @@
-const request = require('request');
+const axios = require('axios').default;
 
 export class RestMachineryService {
     private baseUrl: string;
@@ -12,28 +12,39 @@ export class RestMachineryService {
 
     sendRequest = (type: string, path: string, callback: Function, queryParams?: Map<string, any>, body?: Object, headers?: Map<string, any>) => {
 
-        path ? path = this.baseUrl + path : path = this.baseUrl;
-
-        if (queryParams) {
-            path += this.formatParams(queryParams);
-        }
-
-        let req = {
-            url: path,
-            method: type,
-            headers: headers ? headers : {},
-            body: body ? body : {}
-        };
-
         console.log(new Date(), type, 'REQUEST:', path, body ? body : '');
 
-        request(req, function (error: any, response: any, body: any) {
-            if (error) {
-                callback(response.statusCode, error);
-            } else {
-                callback(response.statusCode, body);
-           }
-        });
+        axios({
+            method: type,
+            baseUrl: this.baseUrl,
+            url: path,
+            params: queryParams,
+            data: body,
+            headers: headers,
+            responseType: 'json',
+            validateStatus: function (status: any) {
+                return status >= 200 && status < 300;
+            }
+        })
+            .then(function (response: any) {
+                callback(response.status, response.data);
+            })
+            .catch(function (error: any) {
+                if (error.response) {
+                    callback(error.response.status, error.response.data);
+                } else if (error.request) {
+                    callback(504, {
+                        'success': false,
+                        'message': 'Request timed out'
+                    });
+                } else {
+                    callback(400, {
+                        'success': false,
+                        'message': 'Something went wrong while trying to perform request'
+                    });
+                }
+            });
+
     }
 
     formatParams = (queryParams: Map<string, any>) => {
