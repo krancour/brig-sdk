ce375c3935ad731f4ec69cc9186f8acbb530b8e1
Cody Uhi <codylkuhi@gmail.com>
2020-12-22 18:55:15 -0700

    Finalize CreateEvent functionality

--- src/core/events-client.ts
+++ src/core/events-client.ts
@@ -24,6 +24,7 @@ export class EventsClient {
 
   createEvent = (projectId: string, source: string, type: string, worker: Worker, eventId?: string, callback?: (status: Number, response: Object) => any) => {
     this.rms.sendRequest('POST', '/v2/events', callback, undefined, {
+      projectID: projectId,
       apiVersion: apiVersion,
       kind: 'Event',
       metadata: {},
